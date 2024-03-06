b74a90a16fd6cde4feae3ef1d453d8bdf1f79dda
umiswing <umiswing@foxmail.com>
2024-03-06 10:25:32 +0000

    Add seqlen_head_dim parallel.

--- source/nmt/submodel/UpdateState.cu
+++ source/nmt/submodel/UpdateState.cu
@@ -27,23 +27,30 @@ namespace nmt {
   __global__ void updateStateKernel(const float* const src,
                                     const int* const index,
                                     const UpdateStateParams params,
+                                    const int split_size,
                                     float * const tgt) {
     // TODO(umiswing): niutensor doesn't guarantee 128-bit aligned.
     // Use 128-bit loading after 128-bit aligned support.
     constexpr int elements_per_thread = 1;
     const int threads_per_block = blockDim.x;
     const int elements_per_block = elements_per_thread * threads_per_block;
+    // split_size for current tb
+    const int current_split_size = blockIdx.x == gridDim.x-1 ?
+                                   params.seqlen * params.head_dim - blockIdx.x * split_size :
+                                   split_size;
   
-    const int src_hid = blockIdx.y;
-    const int src_bid = index[blockIdx.x];
-    const int src_hbid = (src_hid * params.src_batch_size + src_bid) * params.seqlen * params.head_dim;
+    const int src_hid = blockIdx.z;
+    const int src_bid = index[blockIdx.y];
+    const int src_split_id = blockIdx.x;
+    const int src_hb_split_id = (src_hid * params.src_batch_size + src_bid) * params.seqlen * params.head_dim + src_split_id * split_size;
   
-    const int tgt_hid = blockIdx.y;
-    const int tgt_bid = blockIdx.x;
-    const int tgt_hbid = (tgt_hid * params.tgt_batch_size + tgt_bid) * params.seqlen * params.head_dim;
+    const int tgt_hid = blockIdx.z;
+    const int tgt_bid = blockIdx.y;
+    const int tgt_split_id = blockIdx.x;
+    const int tgt_hb_split_id = (tgt_hid * params.tgt_batch_size + tgt_bid) * params.seqlen * params.head_dim + tgt_split_id * split_size;
     #pragma unroll
-    for(int i=threadIdx.x*elements_per_thread;i<params.seqlen * params.head_dim / elements_per_thread;i+=elements_per_block) {
-      tgt[tgt_hbid+i] = src[src_hbid+i];
+    for(int i=threadIdx.x*elements_per_thread;i<current_split_size;i+=elements_per_block) {
+      tgt[tgt_hb_split_id+i] = src[src_hb_split_id+i];
     }
   }
 
@@ -66,15 +73,26 @@ namespace nmt {
     int devIDBackup;
     ProtectCudaDev(devID, devIDBackup);
 
-    dim3 blocks(params.tgt_batch_size, params.num_head);
+    // shorthand for seqlen*head_dim
+    const auto sh_num = params.seqlen * params.head_dim;
 
-    dim3 threads(params.seqlen*params.head_dim > GDevs.GPUs[devID].GPUMaxThreadNumPerBlock ?
-                 GDevs.GPUs[devID].GPUMaxThreadNumPerBlock :
-                 params.seqlen*params.head_dim );
+    // split along (L, H)
+    // TODO(umiswing): add heuristic (maybe)
+    const int loads_per_block = 128*128;
+    const int max_split_size = GDevs.GPUs[devID].GPUMaxThreadNumPerBlock * loads_per_block;
+    const int split_size = sh_num > max_split_size ? max_split_size : sh_num;
+    const int num_split = (sh_num + split_size - 1) / split_size;
 
+    dim3 blocks(num_split, params.tgt_batch_size, params.num_head);
+
+    dim3 threads((split_size + loads_per_block - 1) / loads_per_block);
+
+    // Pzzzzz5142: why not thrust?
+    // umiswing: just for fun. and this kernel will be removed in the future.
     updateStateKernel<<<blocks, threads>>>(static_cast<float*>(src->data),
                                            static_cast<int*>(index->data),
                                            params,
+                                           split_size,
                                            static_cast<float*>(tgt->data));
 
     BacktoCudaDev(devID, devIDBackup);
